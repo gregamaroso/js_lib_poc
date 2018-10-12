8670a39c654b90eba4fd796072c05f49d06593f6
Greg Amaroso <greg@Gregs-MBP.home>
2018-10-12 09:38:54 -0400

    adding v-2 temporarily

---
+++ v-button-2/src/views/main-content.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="v-add-to-bag">
+  <button
+    :data-sku="sku"
+    :data-qty="qty"
+    v-if="displayable"
+    v-on:click="addToBag">{{ label }}</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "v-add-to-bag",
+  data: () => ({
+    qty: 1,
+    sku: 'SKU111',
+    label: 'Add To Cart',
+    displayable: true
+  }),
+  methods: {
+    addToBag: function(event) {
+      console.log(this.qty, this.sku)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.v-add-to-bag button {
+  padding: 4px 8px;
+}
+</style>
+
